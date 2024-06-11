a0444b710a094277e87ccf39bcb2d8b1b5c7046d
b-ma <benjamin.matuszewski@gmail.com>
2024-06-11 12:29:27 +0200

    feat: completely refactor

--- src/browser.js
+++ src/browser.js
@@ -1,49 +1,98 @@
-import { isPlainObject, isString } from '@ircam/sc-utils';
-import urlJoin from 'url-join';
+import {
+  createAudioBufferLoader
+} from './AudioBufferLoader.js';
+import {
+  isURL,
+} from './utils.js';
 
-const contexts = new Map();
-
-export async function loadAudioBuffer(pathname, sampleRate = 48000) {
-  if (!contexts.has(sampleRate)) {
-    const context = new OfflineAudioContext(1, 1, sampleRate);
-    contexts.set(sampleRate, context);
+/**
+ * Try to resolve different patterns for loading soundfiles
+ * - if serverAddress is null, rely on default fetch behavior
+ * - if serverAddress is not null, try to create an URL from pathname and serverAddress
+ *
+ * Note that this function is exported for testing purposes only.
+ *
+ * @param {string} pathname - Path to the resource to load
+ * @param {string} [serverAddress=null] - An URL from which to build a absolute URL
+ * @return {string|null} - Path relative to `process.cwd` if the file has been found
+ *  on the file system, a valid URL or `null` if all strategies failed.
+ * @throws if serverAddress is not avlid URL.
+ */
+export function resolvePathname(pathname, serverAddress = null) {
+  // ensure given serverAddress is a valid URL
+  if (serverAddress !== null && !isURL(serverAddress)) {
+    throw new TypeError(`Cannot execute 'resolvePathname': Argument 2 ('${serverAddress}') is not null and is not a valid URL`);
   }
 
-  const response = await fetch(pathname);
-  const arrayBuffer = await response.arrayBuffer();
-
-  const context = contexts.get(sampleRate);
-  const audioBuffer = await context.decodeAudioData(arrayBuffer);
-
-  return audioBuffer;
-}
-
-export class AudioBufferLoader {
-  constructor({
-    sampleRate = 48000,
-    baseUrl = '',
-  } = {}) {
-    this.sampleRate = sampleRate;
-    this.baseUrl = baseUrl;
-  }
-
-  async load(list) {
-    if (Array.isArray(list)) {
-      const promises = list.map(filename => {
-        const pathname = urlJoin(this.baseUrl, filename);
-        return loadAudioBuffer(pathname, this.sampleRate);
-      });
-
-      return Promise.all(promises);
-    } else if (isPlainObject(list)) {
-      throw new Error(`[AudioBufferLoader] Invalid argument, object is not supported yet`);
-    } else if (isString(list)) {
-      const filename = list;
-      const pathname = urlJoin(this.baseUrl, filename);
-      return loadAudioBuffer(pathname, this.sampleRate);
-    } else {
-      throw new TypeError(`[AudioBufferLoader] Invalid argument`);
+  if (serverAddress === null) {
+    return pathname;
+  } else {
+    try {
+      const url = new URL(pathname, serverAddress);
+      return url.href;
+    } catch(err) {
+      // could not build the URL
+      console.warn(`Cannot resolve path for value '${value}', ignoring...`);
+      return null;
     }
   }
 }
 
+/**
+ * Fetch a given file from the network and decode it
+ * @param {BaseAudioContext} audioContext - audio context used to decode the file
+ * @param {string} resolvedPathname - sanitized pathname as returned by `resolvePathname`
+ * @param {AbortSignal} signal - signal to abort loading
+ * @return {AudioBuffer|null} - Decoded audio buffer or null if aborted or failed
+ */
+async function loadFile(audioContext, resolvedPathname, signal) {
+  if (signal.aborted) {
+    return null;
+  }
+
+  let arrayBuffer;
+
+  try {
+    const response = await fetch(resolvedPathname, { signal });
+
+    if (response.ok) {
+      arrayBuffer = await response.arrayBuffer();
+    } else {
+      console.error(`${response.status} Error: Cannot fetch file '${resolvedPathname}'`);
+      return null;
+    }
+  } catch (err) {
+    if (err.name !== 'AbortError') {
+      console.error(err.message);
+    }
+    return null;
+  }
+
+  if (signal.aborted) {
+    return null;
+  }
+
+  let audioBuffer;
+
+  try {
+    audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+
+  if (signal.aborted) {
+    return null;
+  }
+
+  return audioBuffer;
+}
+
+// create the AudioBufferLoader class with platform specific functionality
+export const AudioBufferLoader = createAudioBufferLoader(
+  window.BaseAudioContext,
+  window.OfflineAudioContext,
+  window.AudioBuffer,
+  resolvePathname,
+  loadFile,
+);
--- src/node.js
+++ src/node.js
@@ -1,89 +1,152 @@
+import fs from 'node:fs';
+import { readFile } from 'node:fs/promises';
 import path from 'node:path';
-import { URL, pathToFileURL } from 'node:url';
-import { default as fs, promises as fsPromises } from 'node:fs';
-import http from 'node:http';
-import https from 'node:https';
 
-import { isPlainObject, isString } from '@ircam/sc-utils';
-import { OfflineAudioContext } from 'node-web-audio-api';
-import { temporaryFileTask } from 'tempy';
+import caller from 'caller';
+import fetch from 'node-fetch';
+import {
+  AudioBuffer,
+  BaseAudioContext,
+  OfflineAudioContext,
+} from 'node-web-audio-api';
 
-const contexts = new Map();
+import {
+  createAudioBufferLoader
+} from './AudioBufferLoader.js';
+import {
+  isURL,
+} from './utils.js';
 
-export async function loadAudioBuffer(pathname, sampleRate = 48000) {
-  if (!contexts.has(sampleRate)) {
-    const context = new OfflineAudioContext(1, 1, sampleRate);
-    contexts.set(sampleRate, context);
+/**
+ * Try to resolve different patterns for loading soundfiles
+ * - filesystem relative to `process.cwd()`
+ * - filesystem relative to caller site
+ * - online absolute URLs
+ * - if `serverAddress` is given and all other strategies failed, try to build
+ *   a valid URL from `pathname` and `serverAddress`
+ *
+ * Note that this function is exported for testing purposes only.
+ *
+ * @param {string} pathname - Path to the resource to load
+ * @param {string} [serverAddress=null] - An URL from which to build a absolute URL
+ * @return {string|null} - Path relative to `process.cwd` if the file has been found
+ *  on the file system, a valid URL or `null` if all strategies failed.
+ * @throws if serverAddress is not avlid URL.
+ */
+export function resolvePathname(pathname, serverAddress = null) {
+  // ensure given serverAddress is a valid URL
+  if (serverAddress !== null && !isURL(serverAddress)) {
+    throw new TypeError(`Cannot execute 'resolvePathname': Argument 2 ('${serverAddress}') is not null and is not a valid URL`);
   }
 
-  const context = contexts.get(sampleRate);
+  // pathname is an absolute URL, just return it
+  if (pathname.startsWith('http')) {
+    return pathname;
+  }
 
-  // try load from file system
-  if (fs.existsSync(pathname)) {
-    const rawBuffer = await fsPromises.readFile(pathname);
-    return context.decodeAudioData(rawBuffer.buffer);
-  // try load from network
+  // try to find given pathname on filesystem, either relative to cwd or to caller site
+  const normPathname = path.normalize(pathname);
+
+  // test if file relative to `process.cwd()` is found
+  if (fs.existsSync(normPathname)) {
+    return path.relative(process.cwd(), normPathname);
   } else {
-    return new Promise((resolve, reject) => {
-      const extension = path.extname(pathname).replace(/^\./, ''); // remove leading '.'
-      const protocol = new URL(pathname).protocol.replace(/:$/, ''); // remove trailing ":"
-      const http_ = protocol === 'http' ? http : https;
+    // test if file exists relative to caller site
+    const depth = process.env.SC_LOADER_RESOLVE_TEST === '1' ? 1 : 2;
+    const callerSite = caller(depth);
+    const dirname = path.dirname(callerSite.replace('file://', ''));
+    const absPathname = path.join(dirname, normPathname);
 
-      temporaryFileTask(async (tempfile) => {
-        return new Promise(async (resolveTask, _reject) => {
-          const file = fs.createWriteStream(tempfile);
+    if (fs.existsSync(absPathname)) {
+      // return path relative to cwd
+      return path.relative(process.cwd(), absPathname);
+    }
 
-          const request = http_.get(pathname, { rejectUnauthorized: false }, (response) => {
-            const { statusCode } = response;
-            if (statusCode !== 200) {
-              reject(new Error(`File not found`));
-            }
+    // File not found on file system but address is given, create an URL from these informations
+    if (serverAddress !== null) {
+      try {
+        const url = new URL(pathname, serverAddress);
+        return url.href;
+      } catch(err) {
+        // could not build the URL
+        console.warn(`Cannot resolve path for value '${value}', ignoring...`);
+        return null;
+      }
+    }
 
-            response.pipe(file);
-            // after download completed close filestream
-            file.on("finish", async () => {
-              file.close();
-
-              const rawBuffer = await fsPromises.readFile(tempfile);
-              const audioBuffer = await context.decodeAudioData(rawBuffer.buffer);
-
-              resolve(audioBuffer);
-              resolveTask(); // clean tmp file
-            });
-          });
-        });
-      }, { extension });
-    });
+    // return null in all other cases
+    console.warn(`Cannot resolve path for value '${value}', ignoring...`);
+    return null;
   }
 }
 
-// @todo - review so that it more smart...
-// Should check if file exist in fs before concatenating `baseUrl`
-export class AudioBufferLoader {
-  constructor({
-    sampleRate = 48000,
-    baseUrl = '',
-  } = {}) {
-    this.sampleRate = sampleRate;
-    this.baseUrl = baseUrl;
+/**
+ * Fetch a given file from the network or from the filesystem and decode it
+ * @param {BaseAudioContext} audioContext - audio context used to decode the file
+ * @param {string} resolvedPathname - sanitized pathname as returned by `resolvePathname`
+ * @param {AbortSignal} signal - signal to abort loading
+ * @return {AudioBuffer|null} - Decoded audio buffer or null if aborted or failed
+ */
+async function loadFile(audioContext, resolvedPathname, signal) {
+  if (signal.aborted) {
+    return null;
   }
 
-  async load(list) {
-    if (Array.isArray(list)) {
-      const promises = list.map(filename => {
-        const pathname = path.join(this.baseUrl, filename);
-        return loadAudioBuffer(pathname, this.sampleRate);
-      });
+  let arrayBuffer;
 
-      return Promise.all(promises);
-    } else if (isPlainObject(list)) {
-      throw new Error(`[AudioBufferLoader] Invalid argument, object is not supported yet`);
-    } else if (isString(list)) {
-      const filename = list;
-      const pathname = path.join(this.baseUrl, filename);
-      return loadAudioBuffer(pathname, this.sampleRate);
-    } else {
-      throw new TypeError(`[AudioBufferLoader] Invalid argument`);
+  if (resolvedPathname.startsWith('http')) {
+    try {
+      const response = await fetch(resolvedPathname, { signal });
+
+      if (response.ok) {
+        arrayBuffer = await response.arrayBuffer();
+      } else {
+        console.error(`${response.status} Error: Cannot fetch file '${resolvedPathname}'`);
+        return null;
+      }
+    } catch (err) {
+      if (err.name !== 'AbortError') {
+        console.error(err.message);
+      }
+      return null;
+    }
+  } else {
+    try {
+      const contents = await readFile(resolvedPathname, { signal });
+      arrayBuffer = contents.buffer;
+    } catch (err) {
+      if (err.name !== 'AbortError') {
+        console.error(err.message);
+      }
+      return null;
     }
   }
+
+  if (signal.aborted) {
+    return null;
+  }
+
+  let audioBuffer;
+
+  try {
+    audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+
+  if (signal.aborted) {
+    return null;
+  }
+
+  return audioBuffer;
 }
+
+// create the AudioBufferLoader class with platform specific functionality
+export const AudioBufferLoader = createAudioBufferLoader(
+  BaseAudioContext,
+  OfflineAudioContext,
+  AudioBuffer,
+  resolvePathname,
+  loadFile,
+);
